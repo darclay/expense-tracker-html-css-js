5b49949cb6c70a0732be9d10cf68570b89b5279b
David Gurule <davidgurulejr@gmail.com>
2022-03-21 15:30:29 -0600

    started on the fetch() request but researching the url for airtable and locating a bearer token

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const dataDiv = document.querySelector("displayTransactionDiv");
+
 const fetchURL = "";
 const apiKey ="";
 
 async function apiRequest(){
-  const getData = await fetch(`${fetchURL}${apiKey}`)
-    .then(()=>{})
-    .then(()=>{})
-    .catch(()=>{})
+  const response = await fetch(`${fetchURL}${apiKey}`)
+    .then((res)=>{
+      if(!res.ok){
+        console.error(`an error occurred with the fetch ${res}`);
+      }else{
+        res.json();
+      }
+    })
+    .then((data)=>{
+
+    })
+    .catch((error)=>{
+      console.error(`an error occurred with `)
+    })
 }
